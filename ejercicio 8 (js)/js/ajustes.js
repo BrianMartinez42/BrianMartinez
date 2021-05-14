5c3aae97f78e4732e9e38af98f869410aba45f87
marbrian <martinezbrian.exe@gmail.com>
2021-05-14 18:44:48 -0300

    Ejercicio 8 terminado

--- ejercicio 8 (js)/js/ajustes.js
+++ ejercicio 8 (js)/js/ajustes.js
@@ -4,9 +4,16 @@ div1.style.color = "#222";
 div1.style.backgroundColor = "#A0A0A0";
 
 function sumar(){
-  var num1 = document.getElementsByName("valor1").value;
-  var num2 = document.getElementsByName('valor2').value;
-  var res = num1 + num2;
-  alert(num1);
-  document.getElementsByName('resultado').value = res;
+  var num1 = Number(document.getElementsByName("valor1")[0].value);
+  var num2 = Number(document.getElementsByName("valor2")[0].value);
+
+  if (num1 == "") {
+    alert("Ingresar el primer numero");
+  } else if (num2 == ""){
+      alert("Ingresar el segundo numero")
+    }
+  else {
+    var res = num1 + num2;
+    Number(document.getElementsByName('resultado')[0].value = res);
+  }
 }
