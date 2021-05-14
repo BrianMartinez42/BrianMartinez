29dbe593cee7aba4beb7a90c65b617ada4e288da
marbrian <martinezbrian.exe@gmail.com>
2021-05-14 15:47:50 -0300

    Actualizar ejercicio 8

--- ejercicio 8 (js)/js/ajustes.js
+++ ejercicio 8 (js)/js/ajustes.js
@@ -2,3 +2,11 @@ var div1 = document.getElementById('divisor');
 
 div1.style.color = "#222";
 div1.style.backgroundColor = "#A0A0A0";
+
+function sumar(){
+  var num1 = document.getElementsByName("valor1").value;
+  var num2 = document.getElementsByName('valor2').value;
+  var res = num1 + num2;
+  alert(num1);
+  document.getElementsByName('resultado').value = res;
+}
