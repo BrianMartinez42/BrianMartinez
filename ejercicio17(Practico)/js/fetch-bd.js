ef9014d89ce9c754c275feead71d66e751eae876
marbrian <martinezbrian.exe@gmail.com>
2021-09-18 01:31:41 -0300

    Actualizo Trabajo Practico y resuelvo error JSON parse

--- ejercicio17(Practico)/js/fetch-bd.js
+++ ejercicio17(Practico)/js/fetch-bd.js
@@ -1,15 +1,50 @@
-function bd() {
-	let dni = document.getElementById("dni");
-	let dniv = dni.value;
-	let conection;
-	conection = new XMLHttpRequest();
-	conection.open("GET","php/conexion.php?dni="+dniv,true);
+var formulario = document.getElementById('Formulario');
+var respuesta = document.getElementById('respuesta');
 
-  conection.onload = function(){
-    if(conection.status === 200){
-      let arreglo = JSON.parse(conection.responseText);
-      console.log(arreglo);
-    }
-  }
-  conection.send();
-}
+	formulario.addEventListener('submit', function(e){
+	  e.preventDefault();
+	  console.log('me diste un click')
+
+	  var datos = new FormData(formulario);
+
+	  console.log(datos)
+	  console.log(datos.get('DNI'))
+	  console.log(datos.get('provincia'))
+		console.log(datos.get('radiob'))
+	  fetch('./php/conexion.php',{
+	    method: 'POST',
+	    body: datos
+	  })
+	    .then( res => res.text())
+	    .then( data => {
+	      console.log(data)
+	      if(data === 'error'){
+	        respuesta.innerHTML = `
+	        <div class="" role="alert" >
+	          <h2>Datos incorrectos.</h2> <br><br>
+						<input type="button" value="Volver a consultar" class="button-blue" onclick="location.reload()"/>
+	        </div>
+	        `
+	      }else {
+	        respuesta.innerHTML = `
+	          ${data}
+						<input type="button" value="Volver a consultar" class="button-blue" onclick="location.reload()"/>
+	        `
+	      }
+	    })
+	})
+	// let conection;
+	// conection = new XMLHttpRequest();
+	// conection.open(
+	// 	"GET",
+	// 	"php/conexion.php?dni="+dniv,
+	// 	true
+	// );
+	//
+  // conection.onload = function(){
+  //   if(conection.status === 200){
+  //     let arreglo = JSON.parse(conection.responseText);
+  //     console.log(arreglo);
+  //   }
+  // }
+  // conection.send();
--- ejercicio17(Practico)/php/conexion.php
+++ ejercicio17(Practico)/php/conexion.php
@@ -1,26 +1,41 @@
 <?php
-  $dni = $_GET['dni'];
-  // $dni = $_POST["DNI"];
-  // $prov = $_POST["Provincia"];
-  // $radio = $_POST["radiob"];
+  $dni = $_POST["DNI"];
+  $prov = $_POST["provincia"];
+  $radio = $_POST["radiob"];
 
   $conexion = mysqli_connect("localhost", "root", "", "padron");
   $consulta =
-  "SELECT pnombre, apellido, cnombre, direccion, numero
-  FROM users JOIN colegio
-  WHERE (users.colegio_id = colegio.id) AND (users.dni='$dni')";
+  ("SELECT personas.nombre, apellido, colegios.nombre, direccion, numero
+  FROM personas JOIN colegios
+  WHERE (personas.colegio_id = colegios.colegio_id) AND (personas.dni='$dni') AND (personas.provincia='$prov') AND (personas.sexo='$radio')");
   $resultado = mysqli_query($conexion, $consulta);
 
-  // $fila = $resultado->fetch_assoc();
   $filas=mysqli_num_rows($resultado);
   if ($filas>0){
     $fila=mysqli_fetch_row($resultado);
-    echo json_encode($fila);
+    echo ('
+        <div class="jumbotron" style="height: auto; width: auto; text-align: center;">
+          <div class="container-fluid">
+            <blockquote class="blockquote">
+              <h3 class="mb-0">'.$fila[1].', '.$fila[0].'</h3>
+            </blockquote><br>
+
+            <p class="lead">Establecimiento</p>
+            <blockquote class="blockquote">
+              <h3 class="mb-0">'.$fila[2].' N° '.$fila[4].'</h3>
+            </blockquote><br>
+
+            <p class="lead">Dirección</p>
+            <blockquote class="blockquote">
+              <h3 class="mb-0">'.$fila[3].'</h3>
+            </blockquote>
+          </div>
+        </div>
+    ');
   }
   else
   {
-    $fila = 0;
-    echo $fila;
+    echo ('error');
   }
 
   // mysqli_free_result($resultado);
